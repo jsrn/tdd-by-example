6d9f528bf8ed3b80b5a0b6d7699abb61da2d58c9
James Nutt <james.nutt@substrakthealth.com>
2020-04-18 14:23:23 +0100

    A currency object should return its amount

--- spec/the_money_example_spec.rb
+++ spec/the_money_example_spec.rb
@@ -1,6 +1,13 @@
 require './lib/dollar.rb'
 
 RSpec.describe Dollar do
+  describe '#amount' do
+    it 'returns the amount stored on the currency object' do
+      dollar = Dollar.new(5)
+      expect(dollar.amount).to eq(5)
+    end
+  end
+
   describe '#times' do
     it 'returns 10 for 5 * 2' do
       dollar = Dollar.new(5)
